b136bf2a92e54a8a5cb29ef21eaaa79f1a32016d
Bryan Astacio <brya0196@gmail.com>
2024-04-10 19:10:06 -0400

    feat: installing packages in the project

--- packages/common/src/index.tsx
+++ packages/common/src/index.tsx
@@ -1,4 +1,5 @@
-import {Text, Pressable} from "react-native";
+import {Text, Pressable, StyleSheet} from "react-native";
+import normalize from "react-native-normalize";
 
 export function HelloWorld() {
   const handlePress = () => {
@@ -6,8 +7,20 @@ export function HelloWorld() {
   }
 
   return (
-    <Pressable onPress={handlePress}>
-      <Text>Hello World 1</Text>
+    <Pressable style={styles.button} onPress={handlePress}>
+      <Text style={styles.buttonText}>Basic Button</Text>
     </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    paddingVertical: normalize(10),
+    paddingHorizontal: normalize(16),
+    backgroundColor: 'lightblue',
+  },
+  buttonText: {
+    color: "#000",
+    fontWeight: '600',
+  }
+});
